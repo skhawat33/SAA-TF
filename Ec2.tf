61c90b7ab8def73c5d9d19556a8bc265cbf4760f
S33-Dev <183328520+skhawat33@users.noreply.github.com>
2026-02-01 22:36:07 +0500

    disable api termination ko commint kar diya

--- Ec2.tf
+++ Ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "Web_server" {
   # --- 🛡️ Reliability Pillar: Accidental Deletion Protection ---
   # AWS Whitepaper point: Human error se bachata hai.
   # SAA-C03: Jab ye true ho, to Console/CLI/Terraform se termination block ho jati hai.
-  disable_api_termination = false 
+  # disable_api_termination = true 
   # agr chahe k user date change hone par instace tefrminate kar k ne create kre ga
   user_data_replace_on_change = true
   vpc_security_group_ids = [aws_security_group.webTFsg.id]
